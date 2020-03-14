fcc2840613e085d0559feea575b7aa673df2bf73
louicoder <musanje2010@gmail.com>
2020-03-14 11:07:47 +0300

    add relevant files and packages

---
+++ src/Components/GetAndEdit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GetAndEdit () {
+  return (
+    <div>
+      <h1>Testing Get and Edit</h1>
+    </div>
+  );
+}
---
+++ src/Components/ShowAndSend.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ShowAndSend () {
+  return (
+    <div>
+      <h1>Show and send</h1>
+    </div>
+  );
+}

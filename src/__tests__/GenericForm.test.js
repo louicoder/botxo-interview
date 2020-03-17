1a0ec848b2b1d850b508f918233b0ebea6b4a045
louicoder <musanje2010@gmail.com>
2020-03-17 11:49:30 +0300

    add tests for GenericForm component

--- src/__tests__/GenericForm.test.js
+++ src/__tests__/GenericForm.test.js
@@ -1,18 +1,49 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
-import mockStore from '../Store/MockStore';
-import * as ReactRedux from 'react-redux';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import GenericForm from '../Components/GenericForm';
 import UserComponent from '../Components/UserComponent';
-import { useDispatch } from 'react-redux';
-import { addGenericUser } from '../store/ActionCreators';
+import MockStore from '../Store/MockStore';
 
 describe('<GenericForm /> component', () => {
-  let state;
+  let store, wrapper, props;
 
-  it('Render without crashing', () => {});
+  beforeEach(() => {
+    store = MockStore({ genericUsers: [], specificUsers: [], loading: false });
+    props = {
+      changeName: jest.fn(),
+      setName: jest.fn(),
+      addToGenericList: jest.fn(),
+      name: '',
+      age: 0,
+      setAge: jest.fn()
+    };
+    wrapper = mount(
+      <Provider store={store}>
+        <GenericForm {...props} />
+      </Provider>
+    );
+  });
 
-  it('renders with the right store state', () => {});
+  it('Render without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-  it('switches from one view to another', () => {});
+  it('should call ADD_GENERIC_USER_ACTION on button click while name and age exist', () => {
+    wrapper.find(UserComponent).find('input').first().simulate('change', { target: { value: 'louis' } });
+    wrapper.find(UserComponent).find('input').last().simulate('change', { target: { value: 20 } });
+
+    wrapper.find('button').simulate('click');
+    expect(store.getActions()).toEqual([ { type: 'ADD_GENERIC_USER_ACTION', name: 'louis', age: 20 } ]);
+  });
+
+  it('simulate a change name event on child component <UserComponent />', () => {
+    wrapper.find(UserComponent).find('input').first().simulate('change', { target: { value: 'louis' } });
+    expect(wrapper.find(UserComponent).find('input').first().props().value).toEqual('louis');
+  });
+
+  it('simulate a change age event on child component <UserComponent />', () => {
+    wrapper.find(UserComponent).find('input').last().simulate('change', { target: { value: 20 } });
+    expect(wrapper.find(UserComponent).find('input').last().props().value).toEqual(20);
+  });
 });
